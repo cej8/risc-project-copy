f8d0b0f29125f217a63a10da36049bdc1e748ff4
Cecile <ces120@duke.edu>
2020-03-06 12:02:26 -0500

    Modified tests and HumanPlayer to fix null pointer exception error from socket

---
+++ client/src/main/java/edu/duke/ece651/risc/client/ClientInputInterface.java
@@ -0,0 +1,6 @@
+package edu.duke.ece651.risc.client;
+import java.io.InputStream;
+
+public interface ClientInputInterface {
+  String readInput(InputStream input);
+}
--- shared/src/test/java/edu/duke/ece651/risc/shared/HumanPlayerTest.java
+++ shared/src/test/java/edu/duke/ece651/risc/shared/HumanPlayerTest.java
@@ -9,21 +9,21 @@ import org.junit.jupiter.api.Test;
 public class HumanPlayerTest {
   @Test
   public void test_PlayerCreation() {
-    /* Socket s = new Socket();
-    HumanPlayer human = new HumanPlayer();
-    try{
-      AbstractPlayer h = new HumanPlayer("Human1",s);
-      //  h.setName("Human1");
-      // h.setPlaying(true);
-      human.setSocket(s);
+    Socket s = new Socket();
+    //   HumanPlayer human = new HumanPlayer();
+    //   try{
+      AbstractPlayer h = new HumanPlayer("Human1");
+       h.setName("Human1");
+       h.setPlaying(true);
+       //    human.setSocket(s);
       assertEquals("Human1",h.getName());
       assertEquals(true, h.isPlaying());
-      assertEquals(s, human .getSocket());
-      assertEquals(true, human.isWatching());
-    }
-    catch(IOException e){
-      e.printStackTrace(System.out);
-    }*/
+      // assertEquals(s, human .getSocket());
+      //  assertEquals(true, h.isWatching());
+      //   }
+    // catch(IOException e){
+    //   e.printStackTrace(System.out);
+    // }
     
     
   }
