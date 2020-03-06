51c20506b78c36078a72e8a1bba4c3c08fe7d599
ld170 <ld170@duke.edu>
2020-03-06 00:18:42 -0500

    create what board looks like

--- server/src/main/java/edu/duke/ece651/risc/server/ParentServer.java
+++ server/src/main/java/edu/duke/ece651/risc/server/ParentServer.java
@@ -42,6 +42,149 @@ public class ParentServer {
   public void addPlayer(ChildServer c){
     children.add(c);
   }
+  // Generate static board - generate tree to represent board
+  public void createBoard(){
+    List<Region> adjRegion = new ArrayList<Region>();
+    List<Region> regionList = new ArrayList<Region>();
+    Region regionA = new Region();
+    Region regionB = new Region();
+    Region regionC = new Region();
+    Region regionD = new Region();
+    Region regionE = new Region();
+    Region regionF = new Region();
+    Region regionG = new Region();
+    Region regionH = new Region();
+    Region regionI = new Region();
+    Region regionJ = new Region();
+    Region regionK = new Region();
+    Region regionL = new Region();
+    Region regionM = new Region();
+    Region regionN = new Region();
+    Region regionO = new Region();
+    // A
+    adjRegion.add(regionB);
+    regionA.setAdjRegions(adjRegion);
+    regionList.add(regionA);
+    // B
+    adjRegion = new ArrayList<Region>();
+    adjRegion.add(regionA);
+    adjRegion.add(regionC);
+    adjRegion.add(regionE);
+    regionB.setAdjRegions(adjRegion);
+    regionList.add(regionB);
+    // C
+    adjRegion = new ArrayList<Region>();
+    adjRegion.add(regionB);
+    adjRegion.add(regionD);
+    adjRegion.add(regionI);
+    regionC.setAdjRegions(adjRegion);
+    regionList.add(regionC);
+    // D
+    adjRegion = new ArrayList<Region>();
+    adjRegion.add(regionC);
+    adjRegion.add(regionF);
+    regionD.setAdjRegions(adjRegion);
+    regionList.add(regionD);
+    // E
+    adjRegion = new ArrayList<Region>();
+    adjRegion.add(regionB);
+    adjRegion.add(regionI);
+    adjRegion.add(regionJ);
+    regionE.setAdjRegions(adjRegion);
+    regionList.add(regionE);
+    // F
+    adjRegion = new ArrayList<Region>();
+    adjRegion.add(regionD);
+    adjRegion.add(regionG);
+    adjRegion.add(regionI);
+    adjRegion.add(regionK);
+    regionF.setAdjRegions(adjRegion);
+    regionList.add(regionF);
+    // G
+    adjRegion = new ArrayList<Region>();
+    adjRegion.add(regionH);
+    adjRegion.add(regionF);
+    regionG.setAdjRegions(adjRegion);
+    regionList.add(regionG);
+    // H
+    adjRegion = new ArrayList<Region>();
+    adjRegion.add(regionG);
+    adjRegion.add(regionK);
+    regionH.setAdjRegions(adjRegion);
+    regionList.add(regionH);
+    // I
+    adjRegion = new ArrayList<Region>();
+    adjRegion.add(regionF);
+    adjRegion.add(regionE);
+    adjRegion.add(regionC);
+    adjRegion.add(regionJ);
+    regionI.setAdjRegions(adjRegion);
+    regionList.add(regionI);
+    // J
+    adjRegion = new ArrayList<Region>();
+    adjRegion.add(regionI);
+    adjRegion.add(regionE);
+    adjRegion.add(regionL);
+    regionJ.setAdjRegions(adjRegion);
+    regionList.add(regionJ);
+    // K
+    adjRegion = new ArrayList<Region>();
+    adjRegion.add(regionF);
+    adjRegion.add(regionH);
+    adjRegion.add(regionO);
+    adjRegion.add(regionL);
+    regionK.setAdjRegions(adjRegion);
+    regionList.add(regionK);
+    // L
+    adjRegion = new ArrayList<Region>();
+    adjRegion.add(regionM);
+    adjRegion.add(regionN);
+    adjRegion.add(regionJ);
+    adjRegion.add(regionK);
+    regionL.setAdjRegions(adjRegion);
+    regionList.add(regionL);
+    // M
+    adjRegion = new ArrayList<Region>();
+    adjRegion.add(regionL);
+    regionM.setAdjRegions(adjRegion);
+    regionList.add(regionM);
+    // N
+    adjRegion = new ArrayList<Region>();
+    adjRegion.add(regionO);
+    adjRegion.add(regionL);
+    regionN.setAdjRegions(adjRegion);
+    regionList.add(regionN);
+    // O
+    adjRegion = new ArrayList<Region>();
+    adjRegion.add(regionN);
+    adjRegion.add(regionK);
+    regionO.setAdjRegions(adjRegion);
+    regionList.add(regionO);
+    // Add to board
+    this.board = new Board(regionList);
+    /*
+               A
+              /
+             B   D
+             |\ /|
+             | C |
+             E | F - G
+             |\|/|   |
+             | I |   H
+             |/   \ /  
+             J     K
+              \   / \
+               \ /   \
+                L     O
+               / \   /
+              M   \ /
+                   N
+     */
+  }
+  public void regionAdjHelper(List<Region> adjRegion) {
+    Region temp = new Region();
+    adjRegion.add(temp);
+  }
   // Generate initial board, set region groups based on number of players 
   public void createStartingGroups(){
     //int numPlayers = children.size();
@@ -49,7 +192,6 @@ public class ParentServer {
     int remainder = MAX_REGIONS % numPlayers;
     int placementRegions = MAX_REGIONS - remainder;
     int groupSize = placementRegions / numPlayers;
-    // Board board = new
     List<Region> regionList = new ArrayList<Region>();
     // assign no units
     Unit u = new Unit();
