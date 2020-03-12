5285a8316ed364e059c102b1ae916ee6626409a0
Courtney Jones <cej8@duke.edu>
2020-03-12 18:10:38 -0400

    Reviewing Cecile's Merge Request: changed objsFrom() method name to getObjsFromOutputStream(); removed deprecated constructor/methods from HumanPlayer class, as well as Socket field

--- shared/src/main/java/edu/duke/ece651/risc/shared/HumanPlayer.java
+++ shared/src/main/java/edu/duke/ece651/risc/shared/HumanPlayer.java
@@ -4,24 +4,17 @@ import java.net.*;
 import java.io.*;
 
 public class HumanPlayer extends AbstractPlayer {
-  private Socket socket = null;
   private static final long serialVersionUID = 6L;
 
   public HumanPlayer(){
 
   }
-  public HumanPlayer(String name){//testing construcotr for tests that do no dpend on socket connection
+
+  public HumanPlayer(String name) {//testing construcotr for tests that do no dpend on socket connection
     this.isPlaying = true;
     this.name = name;
   }
-  public HumanPlayer(String name, Socket s) throws IOException{
-    //SHOULD BE DEPRECATED to decouple socket from inputstream and outputstream
-    this.name = name;
-    this.socket = s;
-    this.isPlaying = true;
-    this.outputStream = new ObjectOutputStream(s.getOutputStream());
-    this.inputStream = new ObjectInputStream(s.getInputStream());
-  }
+
   public HumanPlayer(String name, InputStream in, OutputStream out) throws IOException{
     this.name = name;
     this.isPlaying = true;
@@ -29,29 +22,11 @@ public class HumanPlayer extends AbstractPlayer {
     this.inputStream = new ObjectInputStream(in);
   }
 
-public Socket getSocket() {
-	return socket;
-}
 
-public void setSocket(Socket socket) throws IOException{
-  if(this.socket != null){ closeAll(); }
-	this.socket = socket;
-  this.outputStream = new ObjectOutputStream(socket.getOutputStream());
-  this.inputStream = new ObjectInputStream(socket.getInputStream());
-}
- public  boolean isWatching(){
+  public  boolean isWatching(){
    //TODO: implement method
    return true;
  }
 
-  public void closeAll(){
-    super.closeAll();
-    try{
-      socket.close();
-    }
-    catch(IOException e){
-      e.printStackTrace(System.out);
-    }
-  }
   
 }
