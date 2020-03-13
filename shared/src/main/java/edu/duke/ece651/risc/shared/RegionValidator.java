69f50e5a512e1c90295a5061fe72e1ec0634b7b6
Cecile <ces120@duke.edu>
2020-03-13 00:03:29 -0400

    Region validator and test

--- shared/src/main/java/edu/duke/ece651/risc/shared/RegionValidator.java
+++ shared/src/main/java/edu/duke/ece651/risc/shared/RegionValidator.java
@@ -3,42 +3,58 @@ package edu.duke.ece651.risc.shared;
 import java.util.*;
 
 public class RegionValidator implements ValidatorInterface {
-  private boolean isConnected(Region start, Region end){
-    //find a path of connected nodes from start to end
-    for(Region neighbor: start.getAdjRegions()){
-      if(neighbor == end){
-        return true;
+  private boolean hasValidPath(Region start, Region end,Set<Region> visited) {
+    // find a path of connected nodes from start to end
+    // Set<Region> visited = new HashSet<Region>();
+    visited.add(start);
+    for (Region neighbor : start.getAdjRegions()) {
+      if (visited.contains(neighbor)) {
+        continue;// check if already visited
       }
-      return isConnected(neighbor, end);
-    }
-    
-    return false;
-  }
-	@Override
-	public boolean isValidMove(MoveOrder m, Board b) {
-    //owned by the same person
-    List<Region> ownedRegions=b.getPlayerToRegionMap().get(m.getSource().getOwner());
-    for(Region region: ownedRegions){
-      if(region==m.getDestination()){//destination is in source owner's list of regions
-        if(isConnected(m.getSource(), m.getDestination())){
-           //and have path to get there via adjacent regions
+      visited.add(neighbor);
+      if (start.getOwner().getName().equals(neighbor.getOwner().getName())) {// owned by the same player
+
+        if (neighbor == end) {
           return true;
         }
-      }
-    }
-    return false;
-	}
 
-	@Override
-	public boolean isValidPlacement(PlacementOrder p, AbstractPlayer player, Board b) {
-		//check that player ownd the regions they are placing units in
-  //   List<Region> ownedRegions=b.getPlayerToRegionMap().get(p.getDestination().getOwner()player);
-  //   for(Region region: ownedRegions){
-  //     if(region == p.getDestination()){
-  //       return true;
-  //     }
-  //   }
-  	return false;
-	 }
+        return hasValidPath(neighbor, end, visited);
+      }
+
+    }
+
+    return false;
+  }
+
+  @Override
+  public boolean isValidMove(MoveOrder m, Board b) {
+    // owned by the same person
+    // List<Region>
+    // ownedRegions=b.getPlayerToRegionMap().get(m.getSource().getOwner());
+    // for(Region region: ownedRegions){
+    // if(region==m.getDestination()){//destination is in source owner's list of
+    // regions
+    if (hasValidPath(m.getSource(), m.getDestination(), new HashSet<Region>())) {
+      // and have path to get there via adjacent regions
+      return true;
+
+    }
+    // }
+    // }
+    return false;
+  }
+
+  @Override
+  public boolean isValidPlacement(PlacementOrder p, AbstractPlayer player, Board b) {
+    // check that player ownd the regions they are placing units in
+    // List<Region>
+    // ownedRegions=b.getPlayerToRegionMap().get(p.getDestination().getOwner()player);
+    // for(Region region: ownedRegions){
+    // if(region == p.getDestination()){
+    // return true;
+    // }
+    // }
+    return false;
+  }
 
 }
