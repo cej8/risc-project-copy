a37730e91d0e208b85a8f951823b604f1d248df5
Courtney Jones <cej8@duke.edu>
2020-03-14 13:54:48 -0400

    Extracted out method in AttackOrderTest rather than duplicating code

--- shared/src/test/java/edu/duke/ece651/risc/shared/AttackOrderTest.java
+++ shared/src/test/java/edu/duke/ece651/risc/shared/AttackOrderTest.java
@@ -7,30 +7,34 @@ import org.junit.jupiter.api.Test;
 public class AttackOrderTest {
   @Test
   public void test_attack() {
-    // create two regions
-    AbstractPlayer ps = new HumanPlayer("Player 1");
-    Unit us = new Unit(10);
-    AbstractPlayer pd = new HumanPlayer("Player 2");
-    Unit ud = new Unit(5);
-    Region s = new Region(ps, us);
-    Region d = new Region(pd, ud);
-    Unit u = new Unit(5);
-    // attack
-    AttackOrder ao = new AttackOrder(s,d,u);
-    ao.doAction(null);
+    setupTestAttack(10, 5, 5);
   }
   @Test
   public void test_attack2() {
+    setupTestAttack(10, 1, 5);
+  }
+   @Test
+  public void test_attack3() {
+    setupTestAttack(10, 10, 5);
+  }
+
+  //method for creating attacks to test
+  private void setupTestAttack(int numSourceUnits, int numDestUnits, int numAttackUnits) {
     // create two regions
     AbstractPlayer ps = new HumanPlayer("Player 1");
-    Unit us = new Unit(10);
+    Unit us = new Unit(numSourceUnits);
     AbstractPlayer pd = new HumanPlayer("Player 2");
-    Unit ud = new Unit(1);
+    Unit ud = new Unit(numDestUnits);
     Region s = new Region(ps, us);
+    s.setName("Mars");
+    s.setOwner(ps);
     Region d = new Region(pd, ud);
-    Unit u = new Unit(5);
+    d.setName("Venus");
+    d.setOwner(pd);
+    Unit u = new Unit(numAttackUnits);
     // attack
-    AttackOrder ao = new AttackOrder(s,d,u);
+    AttackOrder ao = new AttackOrder(s, d, u);
     ao.doAction(null);
   }
+
 }
