47c7650ad098abbe1eaada9a1d9b34e6af071323
Cecile <ces120@duke.edu>
2020-03-15 17:41:12 -0400

    RegionValidator implementation and tests

--- shared/src/main/java/edu/duke/ece651/risc/shared/ValidatorInterface.java
+++ shared/src/main/java/edu/duke/ece651/risc/shared/ValidatorInterface.java
@@ -2,7 +2,7 @@ package edu.duke.ece651.risc.shared;
 
 public interface ValidatorInterface {
   public boolean isValidMove(MoveOrder m, Board b);
-  // public boolean isValidAttack(MoveOrder m);
+  // public boolean isValidAttack(AttackOrder  a);
   public boolean isValidPlacement(PlacementOrder p,AbstractPlayer player, Board b);
   
 }
