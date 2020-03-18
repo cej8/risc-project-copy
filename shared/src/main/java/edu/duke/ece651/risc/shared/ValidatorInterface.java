2ebe0e2e17dea2090857deb485158132f5b64a63
Courtney Jones <cej8@duke.edu>
2020-03-18 13:19:44 -0400

    Created UnitValidator.java with attacksAreValid and movesAreValid methods, as well as UnitValidatorTest.java to test it. Placement validation needs to wait until validation classes are restructured

--- shared/src/main/java/edu/duke/ece651/risc/shared/ValidatorInterface.java
+++ shared/src/main/java/edu/duke/ece651/risc/shared/ValidatorInterface.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public interface ValidatorInterface {
   //this interface will be used to validate moves on regions and units are valid
-  public boolean movesAreValid(List <MoveOrder> m, Board b);
-  public boolean attacksAreValid(List <AttackOrder> a, Board b);
-  public boolean placementsAreValid(List <PlacementOrder> p, AbstractPlayer player, Board b);
+  public boolean movesAreValid(List <MoveOrder> m);
+  public boolean attacksAreValid(List <AttackOrder> a);
+  public boolean placementsAreValid(List <PlacementOrder> p, AbstractPlayer player);
   
 }
