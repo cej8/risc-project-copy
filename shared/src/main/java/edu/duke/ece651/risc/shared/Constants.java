256027f40067114c2728a965a352c75964cc1d13
ld170 <ld170@duke.edu>
2020-03-20 14:25:37 -0400

    comments in Constants.java

--- shared/src/main/java/edu/duke/ece651/risc/shared/Constants.java
+++ shared/src/main/java/edu/duke/ece651/risc/shared/Constants.java
@@ -2,12 +2,17 @@ package edu.duke.ece651.risc.shared;
 
 // Class to handle program constants
 public final class Constants {
-
+  // time to wait for initial connection of players
   public static final double START_WAIT_MINUTES = 2.5;
+  // time to wait between turns
   public static final double TURN_WAIT_MINUTES = 1;
+  // max number of units per player
   public static final int MAX_UNITS = 15;
+  // max number of players at a given time
   public static final int MAX_PLAYERS = 1;
+  // max number of regions in board
   public static final int MAX_REGIONS = 12;
+  // default port number
   public static final int DEFAULT_PORT = 12345;
 
 }
