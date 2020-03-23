f5e0dcf918355cf834189374ed18d8ee71684f1a
Cecile <ces120@duke.edu>
2020-03-22 22:24:38 -0400

    Top level wrapper classes for client and server

--- client/src/main/java/edu/duke/ece651/risc/client/ClientProgram.java
+++ client/src/main/java/edu/duke/ece651/risc/client/ClientProgram.java
@@ -1,6 +1,7 @@
 package edu.duke.ece651.risc.client;
 
 public class ClientProgram {
+  //top level class for executing client
   void main(){
     Client client = new Client();
     client.playGame();
--- server/src/main/java/edu/duke/ece651/risc/server/ServerProgram.java
+++ server/src/main/java/edu/duke/ece651/risc/server/ServerProgram.java
@@ -1,6 +1,7 @@
 package edu.duke.ece651.risc.server;
 
 public class ServerProgram {
+  //top level wrapper class for running server
   void main(){
     ParentServer ps = new ParentServer();
     ps.run();
