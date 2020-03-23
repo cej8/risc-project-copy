48447cad25885bcb571b034ce85d2eeb1e965f7b
Cecile <ces120@duke.edu>
2020-03-22 22:01:59 -0400

    Added wrappes for top level client and server execution of program

---
+++ client/src/main/java/edu/duke/ece651/risc/client/ClientProgram.java
@@ -0,0 +1,8 @@
+package edu.duke.ece651.risc.client;
+
+public class ClientProgram {
+  void main(){
+    Client client = new Client();
+    client.playGame();
+  }
+}
---
+++ server/src/main/java/edu/duke/ece651/risc/server/ServerProgram.java
@@ -0,0 +1,9 @@
+package edu.duke.ece651.risc.server;
+
+public class ServerProgram {
+  void main(){
+    ParentServer ps = new ParentServer();
+    ps.run();
+
+  }
+}
