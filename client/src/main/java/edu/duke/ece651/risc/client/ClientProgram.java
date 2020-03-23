1941b98a63d9ed1c3c1d90702ce5005c5dd5372e
Cecile <ces120@duke.edu>
2020-03-23 11:27:26 -0400

    Testing host server setup

--- client/src/main/java/edu/duke/ece651/risc/client/ClientProgram.java
+++ client/src/main/java/edu/duke/ece651/risc/client/ClientProgram.java
@@ -3,7 +3,7 @@ package edu.duke.ece651.risc.client;
 public class ClientProgram {
   //top level class for executing client
   public static void main(String[]args){
-    System.out.println("Now running cleint of RISC Game......");
+    System.out.println("Now running client of RISC Game......");
     Client client = new Client();
     client.playGame();
   }
