6d2f39beded8305d85f61eadbd2f45bf28812d0e
Courtney Jones <cej8@duke.edu>
2020-03-26 09:58:43 -0400

    WIP: changed Unit class to include List<Integer> units to hold tech level bonuses

--- shared/src/main/java/edu/duke/ece651/risc/shared/Unit.java
+++ shared/src/main/java/edu/duke/ece651/risc/shared/Unit.java
@@ -1,24 +1,72 @@
 package edu.duke.ece651.risc.shared;
 import java.io.Serializable;
+import java.util.*;
 
 // Class to set number of units for players
 public class Unit implements Serializable {
-  private Integer units;
-  private static final long serialVersionUID = 2L; //more intuitive numbering?
-
-  public Unit(){
-
+  private List<Integer> units;
+  private static Map<Integer, Integer> techToBonusMap;  
+  private static final long serialVersionUID = 2L;
+  static {
+    techToBonusMap = new HashMap<Integer, Integer>();
+    techToBonusMap.put(0, 0);
+    techToBonusMap.put(1, 1);
+    techToBonusMap.put(2, 3);
+    techToBonusMap.put(3, 5);
+    techToBonusMap.put(4, 8);
+    techToBonusMap.put(5, 11);
+    techToBonusMap.put(6, 15);
   }
-  public Unit(Integer u){
-    setUnits(u);
+  
+  public Unit() {
   }
 
+  //Initialize numUnits units all with bonus 0
+  public Unit(Integer numUnits){
+    units = new ArrayList<Integer>();
+    for (Integer i = 0; i < numUnits; i++){
+      units.add(0);
+    }
+  }
+
+  public List<Integer> getUnitList() {
+    return this.units;
+  }
+
+  //returns the bonus of a unit by index
+  public Integer getUnitBonus(int index) {
+    Integer currUnit = units.get(index);
+    return techToBonusMap.get(currUnit);
+  }
+
+  //returns the tech level of a unit by index
+  public Integer getUnitTech(int index) {
+    return units.get(index);
+  }
+  
+  //return total number of units
   public Integer getUnits(){
-    return units;
+    return units.size();
   }
 
-  public void setUnits(Integer u){
-    this.units = u;
+  //TODO -- will need to validate that units.get(index) + increase <= 6... which class should this occur under?
+  private void upgradeUnitsHelper(int index, int increase) {
+    units.set(index, units.get(index) + increase);
+  }
+
+  //sets the first unit of tech level 'tech' to increase by 'increase'
+  public void upgradeUnits(Integer tech, int increase){
+     for (int i = 0; i < this.getUnits(); i++) {
+       if (units.get(i) == tech){
+        upgradeUnitsHelper(i, increase);
+        //TODO -- take away cost here?
+        break;
+      }
+    }
+  }
+
+  public Integer getBonusFromTech(Integer tech){
+    return techToBonusMap.get(tech);
   }
   
 }
--- shared/src/test/java/edu/duke/ece651/risc/shared/TestUnit.java
+++ shared/src/test/java/edu/duke/ece651/risc/shared/TestUnit.java
@@ -1,17 +1,52 @@
 package edu.duke.ece651.risc.shared;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 public class TestUnit {
   @Test
   public void test_unit() {
-    Unit unit = new Unit();
-    Integer num = 12;
-    unit.setUnits(num);
-    Integer response = unit.getUnits();
-    assertEquals(12,response);
+    Unit unit = new Unit(5);
+    assertEquals(5, unit.getUnits());
+    assertEquals(0, unit.getUnitBonus(0));
+    assertEquals(0, unit.getUnitTech(0));
+    assertEquals(0, unit.getBonusFromTech(unit.getUnitTech(0)));
+    unit.upgradeUnits(0, 2); //change index 0 tech from 0 to 2
+    assertEquals(2, unit.getUnitTech(0));
+    assertEquals(3, unit.getUnitBonus(0));
+    unit.upgradeUnits(0, 2); //change index 1 tech (aka the next first unit with level of 0) from 0 to 2
+    assertEquals(2, unit.getUnitTech(1));
+    assertEquals(3, unit.getUnitBonus(1));
+    assertEquals(3, unit.getBonusFromTech(unit.getUnitTech(1)));
+    unit.upgradeUnits(0, 2); //change index 2 tech from 0 to 2
+    assertEquals(2, unit.getUnitTech(2));
+    unit.upgradeUnits(2, 2); //change index 0 tech from 2 to 4
+    assertEquals(4, unit.getUnitTech(0));
+    assertEquals(8, unit.getUnitBonus(0));
+    assertEquals(8, unit.getBonusFromTech(unit.getUnitTech(0)));
+    unit.upgradeUnits(2, 3); //change index 1 tech from 2 to 5
+    assertEquals(5, unit.getUnitTech(1));
+    assertEquals(11, unit.getUnitBonus(1));
+    unit.upgradeUnits(2, 4); //change index 2 tech from 2 to 6
+    assertEquals(6, unit.getUnitTech(2));
+    assertEquals(15, unit.getUnitBonus(2));
+    this.printList(unit);
   }
 
+  public void printList(Unit u) {
+    List<Integer> list = u.getUnitList();
+    System.out.print("{");
+    for (int i = 0; i < u.getUnits(); i++) {
+      System.out.print("(" + u.getUnitTech(i) + ", " + u.getUnitBonus(i) + ")");
+      if (i != (list.size() - 1)) {
+        System.out.print(", ");
+      }
+    }
+    System.out.println("} ");
+  }
+
+ 
 }
