1b900c832dcd181f07e2d5a16f528e2fd1cf3c1c
Courtney Jones <cej8@duke.edu>
2020-03-29 02:07:29 -0400

    Refactored the Unit class to be a list of integer such that the indices represent the tech level (0 through [currently] 12) and the value at each index represents the frequency of units with that tech level

--- shared/src/main/java/edu/duke/ece651/risc/shared/Unit.java
+++ shared/src/main/java/edu/duke/ece651/risc/shared/Unit.java
@@ -8,12 +8,12 @@ import com.google.common.reflect.TypeToken.TypeSet;
 // Class to set number of units for players
 public class Unit implements Serializable {
   private List<Integer> units;
-  private List<Integer> numOfEachType;
   private static Map<Integer, Integer> techToBonusMap;
   private static Map<Integer, String> techToTypeMap;
   private static final long serialVersionUID = 2L;
   static {
-    techToBonusMap = new HashMap<Integer, Integer>();
+    //maps tech level with associated bonus
+    techToBonusMap = new HashMap<Integer, Integer>(); 
     techToBonusMap.put(0, 0);
     techToBonusMap.put(1, 1);
     techToBonusMap.put(2, 3);
@@ -21,6 +21,7 @@ public class Unit implements Serializable {
     techToBonusMap.put(4, 8);
     techToBonusMap.put(5, 11);
     techToBonusMap.put(6, 15);
+    //maps tech level with name of unit type
     techToTypeMap = new HashMap<Integer, String>();
     techToTypeMap.put(0, "Civilian");
     techToTypeMap.put(1, "Student Trainee");
@@ -32,135 +33,99 @@ public class Unit implements Serializable {
   }
 
   public Unit() {
-    units = new ArrayList<Integer>();
-    this.setNumOfEachType(7, 0);
+    this.setEvenDistribution(7, 0);
   }
 
   // Initialize numUnits units all with bonus 0
   public Unit(Integer numUnits) {
-    units = new ArrayList<Integer>();
-    this.setNumOfEachType(7, 0);
+    this.setEvenDistribution(7, 0);
     this.addUnits(numUnits, 0);
   }
 
-  private void setNumOfEachType(Integer numOfTypes, Integer numOfEach) {
-    numOfEachType = new ArrayList<Integer>();
+  //creates list of numOfTypes indices all with value numOfEach, representing an even distriubtion of bonuses
+  private void setEvenDistribution(Integer numOfTypes, Integer numOfEach) {
+    units = new ArrayList<Integer>();
     for (int i = 0; i < numOfTypes; i++) {
-      numOfEachType.add(numOfEach);
+      units.add(numOfEach);
     }
   }
 
+  //adds numUnits all of technology level tech to the list
   public void addUnits(int numUnits, int tech) {
-    for (Integer i = 0; i < numUnits; i++) {
-      units.add(tech); // add numUnits of tech level tech to units list
-      numOfEachType.set(tech, numOfEachType.get(tech) + 1);// add numUnits to the curr value in int tech index of
-                                                           // numOfEachType list
-    }
-    units.sort(Comparator.naturalOrder());
+    units.set(tech, units.get(tech) + numUnits);
   }
 
-  public List<String> getSortedListOfUnitTypes() {
+  //returns list of unit types in this list of units
+  public List<String> getListOfUnitTypes() {
     List<String> types = new ArrayList<String>();
-    Set<String> addedUnitTypes = new HashSet<String>(); // set of unit types that have been added (i.e. starts off empty
-                                                        // bc none added
-    for (Integer i : this.units) {
-      String currentType = this.getUnitTypeFromTech(i);
-      if (!(addedUnitTypes.contains(currentType))) { // if it hasn't been added
-        types.add(currentType);
-        addedUnitTypes.add(currentType); // add to list of printed types
-
+    for (int i = 0; i < units.size(); i++) {
+      if (units.get(i) != 0) {
+        types.add(this.getTypeFromTech(i));
       }
-
     }
     return types;
   }
 
-  // returns sorted list of units
-  public List<Integer> getUnitList() {
-    units.sort(Comparator.naturalOrder());
+  //  //returns set of unit types in this list of units
+  //   public Set<String> getSetOfUnitTypes() { 
+  //   Set<String> set = new HashSet<String>();
+  // for (int i = 0; i < units.size(); i++){
+  //     set.add(this.getUnitTypeFromTech(i));
+  //   }
+  //   return set;
+  // }
+  //  //returns map of unit types to num of unit types 
+  //   public Map<String, Integer> getTypetoNumMap()  {
+  //   Map<String, Integer> unitNumTypeMap = new HashMap<String, Integer>();
+  //     for (int i = 0; i < units.size(); i++){
+  //     String key = techToTypeMap.get(i);
+  //       unitNumTypeMap.put(key, units.get(i));
+  //   }
+  //   return unitNumTypeMap;
+  // } 
+
+  // return total number of units 
+  public Integer getTotalUnits() {
+    Integer total = 0;
+    for (Integer i : units) {
+      total += i;
+    }
+    return total;
+  }
+
+  public List<Integer> getUnits(){
     return this.units;
   }
 
-  public List<Integer> getNumOfEachType() {
-    return this.numOfEachType;
+  public void setUnits(List<Integer> u) {
+    this.units = u;
   }
-
-  public Set<String> getUnitTypeSet() {
-    Set<String> set = new HashSet<String>();
-    for (Integer i : this.units) {
-      set.add(this.getUnitTypeFromTech(i));
-    }
-    return set;
-  }
-
-  public Map<String, Integer> getUnitNumTypeMap() {
-    Map<String, Integer> unitNumTypeMap = new HashMap<String, Integer>();
-    for (Integer i : this.units) {
-      String key = techToTypeMap.get(i);
-      unitNumTypeMap.put(key, this.getUnitTypeCount(key));
-    }
-    return unitNumTypeMap;
-  }
-
-  public Integer getUnitTypeCount(String type) {
-    Integer count = 0;
-    for (Integer i : this.units) {
-      String key = techToTypeMap.get(i);
-      if (key.equalsIgnoreCase(type)) {
-        count++;
+  
+  public List<Integer> getUnitList() {
+    List<Integer> allUnits = new ArrayList<Integer>();
+    for (int i = 0; i < this.units.size(); i++) {
+      for (int j = 0; j < this.units.get(i); j++) {
+        allUnits.add(i);
       }
     }
-    return count;
-  }
-
-  // returns the bonus of a unit by index
-  public String getUnitTypeFromIndex(int index) {
-    Integer currUnit = units.get(index);
-    return techToTypeMap.get(currUnit);
-  }
-
-  // returns the bonus of a unit by index
-  public Integer getUnitBonusFromIndex(int index) {
-    Integer currUnit = units.get(index);
-    return techToBonusMap.get(currUnit);
-  }
-
-  // returns the tech level of a unit by index
-  public Integer getUnitTechFromIndex(int index) {
-    return units.get(index);
-  }
-
-  // return total number of units
-  public Integer getUnits() {
-    return units.size();
-  }
-
-  private void upgradeUnitsHelper(int index, int increase) {
-    Integer tech = units.get(index);
-    numOfEachType.set(tech, numOfEachType.get(tech) - 1); // subtract one from numOfType you're upgrading from
-    numOfEachType.set(tech + increase, numOfEachType.get(tech + increase) + 1); // add one to numOfType you're upgrading
-    units.set(index, tech + increase);
-
-  }
-
-  // sets the first unit of tech level 'tech' to increase by 'increase'
-  public void upgradeUnits(Integer tech, int increase) {
-    for (int i = 0; i < this.getUnits(); i++) {
-      if (units.get(i) == tech) {
-        upgradeUnitsHelper(i, increase);
-        // TODO -- take away cost here?
-        units.sort(Comparator.naturalOrder());
-        break;
-      }
-    }
+    return allUnits;
   }
 
   public Integer getBonusFromTech(Integer tech) {
     return techToBonusMap.get(tech);
   }
 
-  public String getUnitTypeFromTech(Integer tech) {
+  public String getTypeFromTech(Integer tech) {
     return techToTypeMap.get(tech);
   }
 
+
+
+  // sets the first unit of tech level 'tech' to increase by 'increase'
+  public void upgradeUnit(Integer tech, int increase) {
+    units.set(tech, units.get(tech) - 1); // subtract one from numOfType you're upgrading from
+    units.set(tech + increase, units.get(tech + increase) + 1); // add one to numOfType you're upgrading
+  }
+
+  
 }
