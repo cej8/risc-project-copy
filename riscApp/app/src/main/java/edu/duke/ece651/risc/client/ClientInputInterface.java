ad5d131722beecf8cd4aa9319a9d7be747ca77bd
ld170 <ld170@duke.edu>
2020-04-02 13:10:39 -0400

    client symbolic link

--- riscApp/app/src/main/java/edu/duke/ece651/risc/client/ClientInputInterface.java
+++
@@ -1,9 +0,0 @@
-package edu.duke.ece651.risc.client;
-import java.io.InputStream;
-
-//this interface is used to take in user input from any input stream
-//it will return a single string line to be read and handled
-public interface ClientInputInterface {
-  public String readInput();
-  public void close();
-}
