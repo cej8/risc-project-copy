63edf9a1566b1660a6e3c0bd3e06adeb036daa3d
Cecile <ces120@duke.edu>
2020-04-03 15:10:55 -0400

    WIP AttackCombat and test

--- shared/src/test/java/edu/duke/ece651/risc/shared/AttackOrderTest.java
+++ shared/src/test/java/edu/duke/ece651/risc/shared/AttackOrderTest.java
@@ -32,42 +32,62 @@ public class AttackOrderTest {
     int r2Size = r2.getSize();
     assertEquals(18,p1.getResources().getFuelResource().getFuel());
   }
-  // @Test
-  // public void test_attack() {
-  //   setupTestAttack(10, 5, 5);
-  // }
-  // @Test
-  // public void test_attack2() {
-  //   setupTestAttack(10, 1, 5);
-  // }
-  //  @Test
-  // public void test_attack3() {
-  //   setupTestAttack(10, 10, 5);
-  // }
-  // @Test
-  // public void test_attack4(){
-  //   setupTestAttack(2,10,1);
-  // }
+   @Test
+   public void test_attack() {
+      List<Integer> sourceUnits= new ArrayList<Integer>();
+    sourceUnits.add(1);
+    sourceUnits.add(1);
+    sourceUnits.add(1);
+    sourceUnits.add(1);
+    sourceUnits.add(1);
+    sourceUnits.add(1);
+    sourceUnits.add(1);
+    // create two regions
+    AbstractPlayer ps = new HumanPlayer("Player 1");
+    Unit us = new Unit();
+    us.setUnits(sourceUnits);
+    
+    AbstractPlayer pd = new HumanPlayer("Player 2");
+    Unit ud = new Unit();
+    List<Integer> destUnits= new ArrayList<Integer>();
+    destUnits.add(1);
+    destUnits.add(1);
+    destUnits.add(0);
+    destUnits.add(1);
+    destUnits.add(1);
+    destUnits.add(2);
+    destUnits.add(1);
+    ud.setUnits(destUnits);
+    
+    Region s = new Region(ps, us);
+    s.setName("Mars");
+    Region d = new Region(pd, ud);
+    d.setName("Venus");
+   
+    setupTestAttack(s,d);
+    setupTestAttack(d,s);
+   }
 
   // //method for creating attacks to test
-  // private void setupTestAttack(int numSourceUnits, int numDestUnits, int numAttackUnits) {
-  //   // create two regions
-  //   AbstractPlayer ps = new HumanPlayer("Player 1");
-  //   Unit us = new Unit(numSourceUnits);
-  //   AbstractPlayer pd = new HumanPlayer("Player 2");
-  //   Unit ud = new Unit(numDestUnits);
-  //   Region s = new Region(ps, us);
-  //   s.setName("Mars");
-  //   s.setOwner(ps);
-  //   Region d = new Region(pd, ud);
-  //   d.setName("Venus");
-  //   d.setOwner(pd);
-  //   Unit u = new Unit(numAttackUnits);
-  //   // attack
-  //   AttackOrder ao = new AttackOrder(s, d, u);
-  //   assertEquals(Constants.ATTACK_PRIORITY, ao.getPriority());
-  //   ao.doSourceAction();
-  //   ao.doDestinationAction();
-  // }
+  private void setupTestAttack(Region r1, Region r2) {
+    
+    List<Integer> attackUnits= new ArrayList<Integer>();
+    attackUnits.add(1);
+    attackUnits.add(1);
+    attackUnits.add(0);
+    attackUnits.add(0);
+    attackUnits.add(1);
+    attackUnits.add(0);
+    attackUnits.add(1);
+
+    Unit ua = new Unit();
+    ua.setUnits(attackUnits);
+    // attack
+    AttackMove ap1 = new AttackMove(r1, r2, ua);
+    ap1.doAction();
+    AttackCombat ap2 = new AttackCombat(r1, r1, ua);
+    ap2.doAction();
+   
+  }
 
 }
