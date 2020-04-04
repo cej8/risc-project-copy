fdde3f8390d403ea50016f2d696759ede20d02ff
Courtney Jones <cej8@duke.edu>
2020-04-04 16:48:18 -0400

    Fixed bug that new Unit(something.getUnits().getUnits()) was just returning a pointer to other units rather an a deep copy

--- shared/src/main/java/edu/duke/ece651/risc/shared/UnitBoost.java
+++ shared/src/main/java/edu/duke/ece651/risc/shared/UnitBoost.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 public class UnitBoost extends DestinationOrder {
   private static final long serialVersionUID = 16L;
-  //This class takes in an Unit object representing the number of each type of unit from the region the player wants to boost 1 tech level 
+
+  // This class takes in an Unit object representing the number of each type of
+  // unit from the region the player wants to boost 1 tech level
   public UnitBoost(Region d, Unit u) {
     this.destination = d;
     this.units = u;
@@ -15,21 +17,43 @@ public class UnitBoost extends DestinationOrder {
   @Override
   public String doAction() {
     StringBuilder sb = new StringBuilder(
-        destination.getOwner() + " upgraded units from " + destination.getUnits().getUnits());
-    //create list of new units player will have after boost
-    List<Integer> upgradedUnitList = new ArrayList<Integer>(); //e.g. [0,3,0,0,0,0,0] means after boost, player will have 3 new level 1 units
-    upgradedUnitList.add(0);//no units can't be upgraded to level 0
-    // add [3,0,0,0,0,0] to [0] to get [0,3,0,0,0,0,0]
-    for (int i = 0; i < units.getUnits().size()-1; i++){ //can't upgrade 6
-      upgradedUnitList.add(units.getUnits().get(i));
-    }
-    Unit upgradedUnits = new Unit(upgradedUnitList);
-    //add a new upgraded unit to list
+        destination.getOwner().getName() + " upgraded units from " + destination.getUnits().getUnits());
+    Unit upgradedUnits = getUpgradedUnits();
+    int cost = generateCost();
+    // remove total cost
+    destination.getOwner().getResources().getTechResource().useTech(cost);
+    // add a new upgraded unit to list
     destination.getUnits().addUnits(upgradedUnits);
-    //remove old unit from list
+    // remove old unit from list
     destination.getUnits().subtractUnits(units);
     sb.append(" to " + destination.getUnits().getUnits() + "\n");
     return sb.toString();
+  }
+
+  private Unit getUpgradedUnits() {
+    // create list of new units player will have after boost
+    List<Integer> upgradedUnitList = new ArrayList<Integer>(); // e.g. [0,3,0,0,0,0,0] means after boost, player will
+                                                               // have 3 new level 1 units
+    upgradedUnitList.add(0);// no units can't be upgraded to level 0
+    // add [3,0,0,0,0,0] to [0] to get [0,3,0,0,0,0,0]
+    for (int i = 0; i < units.getUnits().size() - 1; i++) { // can't upgrade 6
+      upgradedUnitList.add(units.getUnits().get(i));
+    }
+    Unit upgradedUnits = new Unit(upgradedUnitList);
+    return upgradedUnits;
+  }
+
+  //generates the cost of any one boost
+  public int generateCost() {
+    int cost = 0;
+    Unit upgradedUnits = getUpgradedUnits(); // get list of new upgraded units
+    // add cost of each upgrade to cost
+    for (int i = 0; i < upgradedUnits.getUnits().size(); i++) {
+      Integer numUnitsofType = upgradedUnits.getUnits().get(i);
+      cost += (upgradedUnits.getCostFromTech(i) * numUnitsofType); // multiply cost to upgrade * num of units of that
+                                                                   // type you're upgrading
+    }
+    return cost;
   }
 
   @Override
