5cab8740f539397e167c11c3f831c65119bcc2c6
Courtney Jones <cej8@duke.edu>
2020-04-06 18:27:26 -0400

    Added max tech level validation to unit boost validation

--- shared/src/test/java/edu/duke/ece651/risc/shared/UnitBoostValidatorTest.java
+++ shared/src/test/java/edu/duke/ece651/risc/shared/UnitBoostValidatorTest.java
@@ -39,16 +39,18 @@ public class UnitBoostValidatorTest {
     Board copy = (Board) DeepCopy.deepCopy(board);
     Region earth = regions.get(0).getRegionByName(copy, "Earth");
     Unit unitBoost = new Unit(listOfUnitInts(0, 0, 0, 0, 0, 5, 0));
+    upgradeTech(earth, 5); //upgrade player tech 5 times 
+
     UnitBoost boost = new UnitBoost(earth, unitBoost);
     ValidatorInterface<UnitBoost> ubv = new UnitBoostValidator(earth.getOwner(), copy);
     List<UnitBoost> orders = new ArrayList<UnitBoost>();
     orders.add(boost); // doesn't have enough units (1 vs 5), fail
     assertEquals(false, ubv.validateOrders(orders)); // false
-    // ubv.validateOrders(orders); // overall fail
 
-    // successive orders suceeding
+    // successive orders failing  -- tech
     Board boardCopy = (Board) DeepCopy.deepCopy(board);
     earth = regions.get(0).getRegionByName(boardCopy, "Earth");
+        upgradeTech(earth, 2); //upgrade player tech 2 times to a level 3
     System.out.println("Earth currently has units " + earth.getUnits().getUnits());
     Unit unitBoost0 = new Unit(listOfUnitInts(1, 0, 0, 0, 0, 0, 0));
     UnitBoost boost0 = new UnitBoost(earth, unitBoost0); // valid: units will be [0, 2, 1, 1, 1, 1, 1]
@@ -59,17 +61,41 @@ public class UnitBoostValidatorTest {
     Unit unitBoost2 = new Unit(listOfUnitInts(0, 0, 3, 0, 0, 0, 0)); // valid: units will be [0, 0, 0, 4, 1, 1, 1]
     UnitBoost boost2 = new UnitBoost(earth, unitBoost2);
     orders.add(boost2);
-    Unit unitBoost3 = new Unit(listOfUnitInts(0, 0, 0, 4, 0, 0, 0)); // valid: units will be [0, 0, 0, 0, 5, 1, 1]
+    Unit unitBoost3 = new Unit(listOfUnitInts(0, 0, 0, 4, 0, 0, 0)); // INvalid: tech level is 3, needs 4 
     UnitBoost boost3 = new UnitBoost(earth, unitBoost3);
     orders.add(boost3);
     ValidatorInterface<UnitBoost> ubvValid = new UnitBoostValidator(earth.getOwner(), boardCopy);
+    assertEquals(false, ubvValid.validateOrders(orders)); //invalID
+
+ // successive orders failing suceeding
+
+    boardCopy = (Board) DeepCopy.deepCopy(board);
+    earth = regions.get(0).getRegionByName(boardCopy, "Earth");
+        upgradeTech(earth, 5); //upgrade player tech 5 times 
+    System.out.println("Earth currently has units " + earth.getUnits().getUnits());
+    unitBoost0 = new Unit(listOfUnitInts(1, 0, 0, 0, 0, 0, 0));
+    boost0 = new UnitBoost(earth, unitBoost0); // valid: units will be [0, 2, 1, 1, 1, 1, 1]
+    orders = new ArrayList<UnitBoost>();
+    orders.add(boost0);
+    unitBoost1 = new Unit(listOfUnitInts(0, 2, 0, 0, 0, 0, 0));
+    boost1 = new UnitBoost(earth, unitBoost1); // valid: units will be [0, 0, 3, 1, 1, 1, 1]
+    orders.add(boost1);
+    unitBoost2 = new Unit(listOfUnitInts(0, 0, 3, 0, 0, 0, 0)); // valid: units will be [0, 0, 0, 4, 1, 1, 1]
+    boost2 = new UnitBoost(earth, unitBoost2);
+    orders.add(boost2);
+    unitBoost3 = new Unit(listOfUnitInts(0, 0, 0, 4, 0, 0, 0)); // valid: units will be [0, 0, 0, 0, 5, 1, 1]
+    boost3 = new UnitBoost(earth, unitBoost3);
+    orders.add(boost3);
+    ubvValid = new UnitBoostValidator(earth.getOwner(), boardCopy);
     assertEquals(true, ubvValid.validateOrders(orders));
+    
     // successive orders failing
     List<UnitBoost> ordersS = new ArrayList<UnitBoost>();
     System.out.println("Earth currently has units " + earth.getUnits().getUnits());
     Unit unitBoost0S = new Unit(listOfUnitInts(1, 0, 0, 0, 0, 0, 0));
     boardCopy = (Board) DeepCopy.deepCopy(board);
     earth = regions.get(0).getRegionByName(boardCopy, "Earth");
+    upgradeTech(earth, 5); //upgrade player tech 5 times 
     UnitBoost boost0S = new UnitBoost(earth, unitBoost0S); // valid: units will be [0, 2, 1, 1, 1, 1, 1]
     ordersS.add(boost0S);
     Unit unitBoost1S = new Unit(listOfUnitInts(0, 2, 0, 0, 0, 0, 0));
@@ -81,6 +107,17 @@ public class UnitBoostValidatorTest {
     ValidatorInterface<UnitBoost> ubvValidS = new UnitBoostValidator(earth.getOwner(), boardCopy);
     assertEquals(false, ubvValidS.validateOrders(ordersS));
 
+  }
+
+  private void upgradeTech(Region earth, int num) {
+    for (int i = 0; i < num; i++) {
+      earth.getOwner().getMaxTechLevel().upgradeLevel();
+    }
+  }
+
+  @Test
+  public void testUBV_tech(){
+
 
   }
 
@@ -88,17 +125,21 @@ public class UnitBoostValidatorTest {
   public void test_UBVcorners() {
     List<Region> regions = getRegions(1, 1, 1, 1, 1, 1); // 1 unit of each type in each region
     Board board = new Board(regions);
-    regions.get(0).getRegionByName(board, "Earth").getOwner().getResources().getTechResource().addTech(300); // 15 + 125 = 140
+    regions.get(0).getRegionByName(board, "Earth").getOwner().getResources().getTechResource().addTech(300); // 15 + 125
+                                                                                                             // = 140
     Board boardCopy = (Board) DeepCopy.deepCopy(board);
     Region earth = regions.get(0).getRegionByName(boardCopy, "Earth");
     UnitBoostValidator ubv = new UnitBoostValidator(earth.getOwner(), boardCopy);
     List<UnitBoost> orders = new ArrayList<UnitBoost>();
     Unit illegalUnits = new Unit(listOfUnitInts(0, 0, 0, 0, 0, 0, 1));
+        upgradeTech(earth, 5); //upgrade player tech 5 times 
     UnitBoost boostUnits = new UnitBoost(earth, illegalUnits);
     orders.add(boostUnits);
     assertEquals(false, ubv.validateOrders(orders)); // false
     boardCopy = (Board) DeepCopy.deepCopy(board);
     earth = regions.get(0).getRegionByName(boardCopy, "Earth");
+        upgradeTech(earth, 5); //upgrade player tech 5 times 
+
     ValidatorInterface<UnitBoost> ubv2 = new UnitBoostValidator(earth.getOwner(), boardCopy);
     List<UnitBoost> orders2 = new ArrayList<UnitBoost>();
     earth.getOwner().getResources().getTechResource().useTech(175); // now player has 140 tech (315-175=140)
@@ -108,6 +149,7 @@ public class UnitBoostValidatorTest {
     assertEquals(true, ubv2.validateOrders(orders2)); // true
     boardCopy = (Board) DeepCopy.deepCopy(board);
     earth = regions.get(0).getRegionByName(boardCopy, "Earth");
+        upgradeTech(earth, 5); //upgrade player tech 5 times 
     ValidatorInterface<UnitBoost> ubv3 = new UnitBoostValidator(earth.getOwner(), boardCopy);
     List<UnitBoost> orders3 = new ArrayList<UnitBoost>();
     earth.getOwner().getResources().getTechResource().useTech(176); // now player only has 139 tech (315-176=139)
