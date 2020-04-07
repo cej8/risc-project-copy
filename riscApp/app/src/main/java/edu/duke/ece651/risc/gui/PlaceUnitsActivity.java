43e44add4d39d89238dfa5bb6e92980d288dbff1
Leighanne Davis <leighanne.davis@duke.edu>
2020-04-06 23:22:40 -0400

    login

---
+++ riscApp/app/src/main/java/edu/duke/ece651/risc/gui/PlaceUnitsActivity.java
@@ -0,0 +1,21 @@
+package edu.duke.ece651.risc.gui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class PlaceUnitsActivity extends AppCompatActivity {
+    TextView helpText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_units);
+    }
+
+    public void sendPlacement(View view){
+        
+    }
+}
