736a2bc41f4489b4ff6fb6db8fca2c46cb79b8bd
Courtney Jones <cej8@duke.edu>
2020-04-17 16:15:08 -0400

    Created PlanetDrawable class to abstract out methods for setting Planet colors based on players

--- riscApp/app/src/main/java/edu/duke/ece651/risc/gui/DisplayMapActivity.java
+++ riscApp/app/src/main/java/edu/duke/ece651/risc/gui/DisplayMapActivity.java
@@ -77,67 +77,11 @@ public class DisplayMapActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        setPlanets();
-    }
-
-    public void setPlanets() {
-        List<AbstractPlayer> players = board.getPlayerList();
-        List<Integer> planetDrawables = getPlanetDrawables();
-        Map<AbstractPlayer, Integer> playerToColorMap = getPlayerToColorMap();
-        Map<Region, ImageButton> regionToButtonMap = getRegionToButtonMap();
-        Map<AbstractPlayer, List<Region>> playerToRegionMap = board.getPlayerToRegionMap();
-        Map<Integer, String> intToColorMap = getIntToColorMap();
-        for (AbstractPlayer p : players) { //for each entry in the map
-            for (Region r : playerToRegionMap.get(p)) { //for each region the player has
-                System.out.println(p.getName() + "'s region " + r.getName() + " is " + intToColorMap.get(playerToColorMap.get(p)));
-                regionToButtonMap.get(r).setBackgroundResource(playerToColorMap.get(p)); //set the drawable of that region's corresponding image button to that player's color
-            }
-        }
-        //set all unowned regions to grey
-        for (Region r : regions) {
-            if (r.getOwner() == null) {
-                regionToButtonMap.get(r).setBackgroundResource(R.drawable.grey_planet);
-            }
-        }
-    }
-
-    public  Map<AbstractPlayer, Integer> getPlayerToColorMap(){
-        List<AbstractPlayer> players = board.getPlayerList();
-        System.out.println("num of players " + players.size());
-        for (AbstractPlayer p : players){
-            System.out.println("Player = " + p.getName());
-        }
-        List<Integer> planetDrawables = getPlanetDrawables();
-        Map<Integer, String> intToColorMap = getIntToColorMap();
-        Map<AbstractPlayer, Integer> playerToColorMap = new HashMap<AbstractPlayer, Integer>();
-        for (int i = 0; i < players.size(); i++){
-            playerToColorMap.put(players.get(i), planetDrawables.get(i));
-            Log.d(players.get(i).getName() + " is color ",  intToColorMap.get(playerToColorMap.get(players.get(i))));
-        }
-        return playerToColorMap;
-    }
-
-    //regions
-    public Map<Region, ImageButton> getRegionToButtonMap(){
-        Map<Region, ImageButton> regionToButtonMap = new HashMap<Region, ImageButton>();
         List<ImageButton> planetButtons = getPlanetButtons();
-        System.out.println("Number of planet buttons " + planetButtons.size());
-        System.out.println("Number of regions " + regions.size());
-        for (int i = 0; i < planetButtons.size(); i++){
-            regionToButtonMap.put(board.getRegions().get(i), planetButtons.get(i));
-        }
-        return regionToButtonMap;
+        PlanetDrawable pd = new PlanetDrawable(board, planetButtons);
+        pd.setPlanets();
     }
 
-    public Map<Integer, String> getIntToColorMap(){
-        Map<Integer, String> map = new HashMap<Integer, String>();
-        map.put(R.drawable.red_planet, "RED");
-        map.put(R.drawable.blue_planet, "BLUE");
-        map.put(R.drawable.green_planet, "GREEN");
-        map.put(R.drawable.sky_planet, "SKY");
-        map.put(R.drawable.pink_planet, "PINK");
-        return map;
-    }
 
     public List<ImageButton> getPlanetButtons(){
         List<ImageButton> planetButtons = new ArrayList<ImageButton>();
@@ -167,23 +111,6 @@ public class DisplayMapActivity extends AppCompatActivity {
         planetButtons.add(planet11);
         return planetButtons;
     }
-
-    public List<Integer> getPlanetDrawables(){
-        Integer redPlanet = (R.drawable.red_planet);
-        Integer bluePlanet = (R.drawable.blue_planet);
-        Integer greenPlanet = (R.drawable.green_planet);
-        Integer skyPlanet = (R.drawable.sky_planet);
-        Integer pinkPlanet = (R.drawable.pink_planet);
-        List<Integer>  planetDrawables = new ArrayList<Integer>();
-        planetDrawables.add(redPlanet);
-        planetDrawables.add(bluePlanet);
-        planetDrawables.add(greenPlanet);
-        planetDrawables.add(skyPlanet);
-        planetDrawables.add(pinkPlanet);
-        //planetDrawables.add(greyPlanet);
-        return planetDrawables;
-    }
-
 
     public void getOrders(){
         Intent i = getIntent();
@@ -329,7 +256,7 @@ public class DisplayMapActivity extends AppCompatActivity {
     
     // Mock board
     public void generateBoard(){
-        List<Region> regions = getRegions(3);
+        List<Region> regions = getRegions(5);
         Board b = new Board(regions);
         ParentActivity parentActivity = new ParentActivity();
         parentActivity.setBoard(b);
