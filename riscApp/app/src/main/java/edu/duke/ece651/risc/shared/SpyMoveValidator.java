b8e737c2fe8f7ed470b801eec85c3aee53a8d741
ces120 <cecile.sadler@duke.edu>
2020-04-27 12:47:48 -0400

    Update GUI shared files for validation in SpyMove, SpyUpgrade, and Cloak

--- riscApp/app/src/main/java/edu/duke/ece651/risc/shared/SpyMoveValidator.java
+++ riscApp/app/src/main/java/edu/duke/ece651/risc/shared/SpyMoveValidator.java
@@ -1,7 +1,10 @@
 package edu.duke.ece651.risc.shared;
 
 import java.util.*;
+
 // Class validates if Spy move is valid
+// Needs to ensure there is a spy in the source region, a path exists to the final region,
+// And hasn't moved into enemy region this turn if will be
 public class SpyMoveValidator implements ValidatorInterface<SpyMoveOrder> {
 
   private Board tempBoard;
@@ -35,13 +38,14 @@ public class SpyMoveValidator implements ValidatorInterface<SpyMoveOrder> {
       }
     }
 
-    return false;    
+    return false;
   }
 
   private boolean isValidMove(SpyMoveOrder move){
     Region start = move.getSource().getRegionByName(tempBoard, move.getSource().getName());
     Region end = move.getDestination().getRegionByName(tempBoard, move.getDestination().getName());
     AbstractPlayer player = move.getPlayer();
+    SpyMoveOrder spyMoveCopy = new SpyMoveOrder(start, end, player);
     //There are no spies in region
     if(start.getSpies(player.getName()).size() == 0){
       return false;
@@ -60,7 +64,7 @@ public class SpyMoveValidator implements ValidatorInterface<SpyMoveOrder> {
       return false;
     }
     //Otherwise path exists and is legal, doAction()
-    move.doAction();
+    spyMoveCopy.doAction();
     return true;
   }
 
@@ -73,5 +77,5 @@ public class SpyMoveValidator implements ValidatorInterface<SpyMoveOrder> {
     }
     return true;
   }
-  
+
 }
